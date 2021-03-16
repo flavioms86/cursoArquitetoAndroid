d209a918f9092224dd354889f7019094249f4e0f
Flavio Silva <flavioms86@hotmail.com>
2021-03-16 15:11:25 -0300

    Etapa 10 - Implementando tela de login do usuario

--- app/src/main/java/com/example/appcommerce/UserLoginActivity.kt
+++ app/src/main/java/com/example/appcommerce/UserLoginActivity.kt
@@ -1,30 +1,36 @@
 package com.example.appcommerce
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 
-class UserRegisterActivity : AppCompatActivity() {
+class UserLoginActivity : AppCompatActivity() {
 
     lateinit var toolbar: Toolbar
     lateinit var textTitle: TextView
-
+    lateinit var btnRegister: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_register)
+        setContentView(R.layout.activity_user_login)
 
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         textTitle = findViewById(R.id.toolbar_title)
-        textTitle.text = getString(R.string.user_register_title)
+        textTitle.text = getString(R.string.user_login_title)
+
+        btnRegister = findViewById(R.id.btn_login_register)
+        btnRegister.setOnClickListener {
+            val intent = Intent (this, UserRegisterActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
